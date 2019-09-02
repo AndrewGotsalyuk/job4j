494ff80bbcc502bcfd826c2bfcb1b22028b8f206
AndrewGotsalyuk <darkhorse@bk.ru>
2019-09-02 06:38:49 +0300

    5.4. Протеиновая диета.[#172690]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public int calc(int ivan, int nik) {
+        int month = 0;
+        int powerIvan = 3;
+        int powerNik = 2;
+        while (ivan < nik){
+            ivan *=powerIvan;
+            nik *=powerNik;
+            month ++;
+        }
+        return month;
+    }
+}
+
