31185a6a64ca1ed7f2ca1e11aa7056969726ff1b
AndrewGotsalyuk <darkhorse@bk.ru>
2019-10-09 22:04:13 +0300

    2

--- chapter_001/src/test/java/ru.job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru.job4j/condition/PointTest.java
@@ -3,17 +3,15 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-//public class PointTest {
+public class PointTest {
 
-//    @Test
-//    public void distance() {
-//        int x1 = 0;
-//        int y1 = 0;
-//        int x2 = 2;
-//        int y2 = 0;
-//        int expected = 2;
-//        int out = (int) Point.distance(x1, y1, x2, y2);
-//        Assert.assertEquals(expected, out);
-//    }
+    @Test
+    public void distance() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        int expected = 2;
+        int out = (int) a.distance(b);
+        Assert.assertEquals(expected, out);
+    }
 
-//}
+}
